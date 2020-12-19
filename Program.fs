7e9b1b3656f3d9ebbb27f3d367bc78d37a778458
null_pointer <nikturab@outlook.com>
2020-12-19 20:46:54 +0300

    Elimination for equations

--- Program.fs
+++ Program.fs
@@ -2,8 +2,10 @@
 
 open System
 
+open System.Collections.Generic
 open Microsoft.Z3
 
+exception ParseError of string
 
 type Term =
     | EXP2 of Term
@@ -29,8 +31,8 @@ type FORMULA =
     | IMPLIES of FORMULA * FORMULA
     | EXISTS of Term * FORMULA  // Term must be only VAR
     | ALL of Term * FORMULA
-    
-//type DNF = 
+
+
 let a = AND(TRUE, FALSE)
 
 
@@ -39,20 +41,9 @@ let is_this_variable term varname =
         | VAR v -> v.Equals(varname)
         | _ -> false
 
-let rec unpack conjunction (quantified_variable: string) =
-    let rec find conjunction = 
-        match conjunction with // looking for a literal containing quantified variable: x = 7, x = var2 
-            | AND (_, ((EQUALS (VAR t1, (_ as term))) | (EQUALS ((_ as term), VAR t1)))  ) when t1.Equals(quantified_variable) && not (is_this_variable term quantified_variable)  -> Some term
-            | AND (((EQUALS (VAR t1, (_ as term))) | (EQUALS ((_ as term), VAR t1))), _) when t1.Equals(quantified_variable)  && not (is_this_variable term quantified_variable) -> Some term
-            | AND((AND _ as t1), _)  -> find t1
-            | AND(_, (AND _ as t1))  -> find t1 
-    let term = find conjunction
-    term // todo: replace x with this term
-                                                    
-let rec Eliminate formula =
-    formula
-    
-    
+
+
+                                               
 let term_to_str term =
     match term with
         | VAR name -> name
@@ -63,9 +54,10 @@ let rec formula_to_str formula =
         | OR (a, b) -> ("Or(" + formula_to_str(a)+", "+formula_to_str(b)+")"); 
         | TRUE -> "True"
         | FALSE -> "False"
-        | EQUALS (t1, t2) -> "eq(" + term_to_str(t1) + ", " + term_to_str(t2)+ ")"
+        | EXISTS(VAR name, F) -> "Exists(" + name + ", " + (formula_to_str F) + ")"
+        | EQUALS (t1, t2) ->  term_to_str(t1) + "==" + term_to_str(t2)
 
-let rec DNF formula =
+let rec DNF formula = // приводит бескванторную формулу в DNF
     match formula with
         | AND (p1, p2) -> match DNF p1 with
                               | OR (a1, a2) -> DNF (OR( AND(p2, a1), AND(p2, a2) ))
@@ -74,9 +66,71 @@ let rec DNF formula =
                                              | _ -> AND (p1, p2)
         | OR (d1, d2) -> OR (DNF d1, DNF d2)                                     
         | (TRUE | FALSE | (EQUALS _)) as literal  -> literal
-        | (ALL _ | EXISTS _ | IMPLIES _ | NOT _ | LT _ | GT _) -> printf "OR, AND, = are only available"; TRUE
+        | _ -> raise (ParseError "OR, AND, = are only available")
         
+let simplify formula =
+    match formula with
+        | AND(TRUE, v1) -> v1
+        | AND(v1, TRUE) -> v1
+        | AND(_, FALSE) -> FALSE
+        | AND(FALSE, _) -> FALSE
+        | OR(TRUE, _) -> TRUE
+        | OR(_, TRUE) -> TRUE
+        | OR(v1, FALSE) -> v1
+        | OR(FALSE, v1) -> v1
+        | v -> v
+        
+let rec Eliminate formula (quantified_variable: string) = // устраняет переменную из бескванторной формулы 
 
+    let rec _Eliminate conjunctions = // устраняет переменную в каждой конъюкции
+
+        let make_conjunction list =
+           let rec loop list (acc: Option<Term>) =
+               match list with
+               | (EQUALS(VAR v1, (_ as v2))) :: tail when v1 = quantified_variable  -> if acc.IsSome then
+                                                                                        simplify (  AND(EQUALS(acc.Value, v2), simplify (loop tail acc)))
+                                                                                       else
+                                                                                        simplify (loop tail (Some v2))
+               | formula :: tail -> simplify (AND( simplify(formula), simplify (loop tail acc)))
+               | [] -> TRUE
+           loop list None
+        
+        let rec make_disjunction conjunctions =
+            match conjunctions with
+                | head :: tail ->  simplify (OR(make_conjunction head, make_disjunction tail))
+                | [] -> FALSE
+                
+        make_disjunction conjunctions
+        
+    let rec flat_conjunction f = // представляет конъюкцию массивом литералов
+       match f with
+            | AND (_ as p1, (_ as p2)) -> (flat_conjunction p1) @ (flat_conjunction p2)
+            | EQUALS(v2, VAR v1) when v1 = quantified_variable -> [EQUALS((VAR v1), v2)] // fix order of terms 
+            | (TRUE | FALSE | (EQUALS _)) as literal -> [literal] 
+            | OR _ -> raise (ParseError "Not DNF")
+            | _ -> raise (ParseError "OR, AND, = are only available")
+                        
+            
+    let rec extract_conjunctions f = // представляет DNF формулу массивом конъюкций
+        match f with
+            | OR (OR _ as p1, (OR _ as p2)) -> (extract_conjunctions p1) @ (extract_conjunctions p2)
+            | OR (_ as p1, (OR _ as p2)) -> (flat_conjunction p1) :: (extract_conjunctions p2)
+            | OR (OR _ as p1, (_ as p2)) -> (flat_conjunction p2) :: (extract_conjunctions p1)
+            | OR (_ as p1, (_ as p2)) -> [flat_conjunction p1] @ [flat_conjunction p2]
+            | other -> [ flat_conjunction other ]
+            | _ -> raise (ParseError "OR, AND, = are only available")
+    
+                
+            
+    formula |> DNF |> extract_conjunctions |> _Eliminate
+   
+let rec EliminateAllQuantifiers formula = // устраняет все кванторы \exists из формулы 
+    match formula with
+        | EXISTS(VAR name, F) -> Eliminate (EliminateAllQuantifiers F) name
+        | (TRUE | FALSE | EQUALS _) as literal -> literal
+        | AND(p1, p2) -> AND(EliminateAllQuantifiers p1, EliminateAllQuantifiers p2)
+        | OR(p1, p2) -> OR(EliminateAllQuantifiers p1, EliminateAllQuantifiers p2)
+        | v -> v
 
 
 [<EntryPoint>]
@@ -87,14 +141,19 @@ let main argv =
     
     let a = VAR "a"
     let b = VAR "b"
+   
     
     
-    let formula = OR(AND(EQUALS(a, b), EQUALS(x, y)), OR(AND(OR(EQUALS(x, b), AND(EQUALS(y, a), EQUALS(x, y))), EQUALS(x, a)), AND(EQUALS(x, y), TRUE)))
+    let formula = EXISTS(x, AND( EXISTS(y, OR(EQUALS(x, y), EQUALS(x, a))), AND(EQUALS(x, a), EQUALS(x, b) )))
     
     let printed_formula = formula_to_str formula
-    let printed_dnf = formula_to_str (DNF formula)
-    
-    printf "%s" printed_formula
-    printf "\n%s" printed_dnf
+    let eliminated_formula = EliminateAllQuantifiers formula
+
+    printf "Your formula: %s\n" printed_formula
+    printf "\nFree formula: %s\n" (formula_to_str (eliminated_formula))
+    (* Example:
+        Your formula: Exists(x, And(Exists(y, Or(x==y, x==a)), And(x==a, x==b)))
+        Free formula: a==b
+    *)
     0
     
