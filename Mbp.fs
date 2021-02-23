85eb2c61bf50ebfac3b02960d5cfdb8f9fbaf521
null_pointer <nikturab@outlook.com>
2021-02-24 02:03:27 +0500

    fix formatting

--- Mbp.fs
+++ Mbp.fs
@@ -57,49 +57,36 @@ let rec MbpZ (bounded_variable: string) (M: Map<string, int>) (cube: Cube) =
                 let make_conjunct2 conjunct =
                     match conjunct with
                     | Upper (num, t) when num <> coefficient_U && t <> term_U ->
-                        Some
-                            ((t
-                              * (Int(LCM / num))
-                              <== term_L * (Int(LCM / coefficient_L))))
+                        Some((t* (Int(LCM / num)) <== term_L * (Int(LCM / coefficient_L))))
                     | Lower (num, t) when num <> coefficient_L && t <> term_L ->
-                        Some
-                            ((term_U
-                              * (Int(LCM / coefficient_U))
-                              <== t * (Int(LCM / num))))
+                        Some((term_U * (Int(LCM / coefficient_U)) <== t * (Int(LCM / num))))
                     | _ -> None
 
-                let c1 =
-                    (l_coeffs, lower_bounds) ||> constraints_on_bounds
+                let c1 = (l_coeffs, lower_bounds) ||> constraints_on_bounds
 
-                let c2 =
-                    (u_coeffs, upper_bounds) ||> constraints_on_bounds
+                let c2 = (u_coeffs, upper_bounds) ||> constraints_on_bounds
 
-                let c3 =
-                    residual.conjuncts
-                    |> (Array.choose (|Bounds|_|))
-                    |> (Array.choose make_conjunct2)
+                let c3 = residual.conjuncts
+                            |> (Array.choose (|Bounds|_|))
+                            |> (Array.choose make_conjunct2)
 
-                let c4 =
-                    [| Div(term_L * (Int(LCM / coefficient_L)), LCM)
-                       <== Div(term_L * (Int(LCM / coefficient_L)), LCM) |]
-
+                let c4 = [| Div(term_L * (Int(LCM / coefficient_L)), LCM) <== Div(term_L * (Int(LCM / coefficient_L)), LCM) |]
                 [ c1; c2; c3; c4 ] |> Array.concat |> Cube
-            | cube -> Cube([| if cube.apply_model M then True else False |])
             | Rule3 (T, conjunct) ->
                 let rew =
                     match T with
                     | Upper_ (f, b, d) ->
-                        [| f <== (d + Term.One) * (Int b) - Term.One
-                           d <== Div(Term.Max, b) |]
+                        [| f <== (d + Term.One) * (Int b) - Term.One ; d <== Div(Term.Max, b) |]
                     | Lower_ (f, y, g) ->
-                        [| (g + Term.One) * (Int y) - Term.One <! f
-                           g <== Div(Term.Max, y) |]
+                        [| (g + Term.One) * (Int y) - Term.One <! f ; g <== Div(Term.Max, y) |]
 
                 [ (Array.except [ conjunct ] cube.conjuncts)
                   rew ]
                 |> Array.concat
                 |> Cube
                 |> (MbpZ bounded_variable M)
+            | cube -> Cube([| if cube.apply_model M then True else False |])
+
 
         open_conjuncts + residual
 
