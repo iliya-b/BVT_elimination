12cf3a2c0003a6bca8a7e889fca012b59433fb3e
null_pointer <nikturab@outlook.com>
2021-06-06 15:36:47 +0300

    Refactor

--- MathHelpers.fs
+++ MathHelpers.fs
@@ -11,9 +11,6 @@ let internal lcm (a, b) = ( (uint64 a) * (uint64 b) |> uint32 ) / gcd (a, b)
 let internal lcmlist list = List.reduce (fun a b -> lcm(a, b)) list
 
 
-open Operators.OperatorIntrinsics
-
-
 let internal pown_2 (power: uint32) = pown (uint64 2) (int32 power)
  
  
